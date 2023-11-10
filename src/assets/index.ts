40a81363708d4435b43d9d080cd1efd6f59e7752
CSP <16367956+CSP197@users.noreply.github.com>
2023-11-10 12:51:05 -0800

    Adding thumbnail click func

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ import image2 from './bird.jpg'
 import image3 from './black-dog.jpg'
 import image4 from './border-collie.jpg'
 import image5 from './zebra.png'
+import up_arrow from './up-arrow.svg'
 
-export { image1, image2, image3, image4, image5 };
+export { image1, image2, image3, image4, image5, up_arrow };
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { up_arrow } from "../assets/index";
 
 interface CarouselProps {
   imageList: {
@@ -13,12 +14,23 @@ const Carousel = (props: CarouselProps) => {
   return (
     <div className="flex justify-center space-x-4 pb-5 sm:p-5">
       {props.imageList.map((image: any, idx: number) => (
-        <img
-          key={idx}
-          className="max-w-full rounded-lg object-scale-down max-h-20"
-          src={image.thumb}
-          onClick={() => props.thumbnailClickFunc(idx)}
-        />
+        <div key={"div_" + idx} className="grid flex-col">
+          <img
+            key={"img_" + idx}
+            className="max-w-full rounded-lg object-scale-down max-h-20"
+            src={image.thumb}
+            onClick={() => props.thumbnailClickFunc(idx)}
+          />
+          {idx === props.activeIndex ? (
+            <img
+              key={"arrow_" + idx}
+              src={up_arrow}
+              className="justify-self-center object-scale-down max-h-3"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
       ))}
     </div>
   );
