5ff7bf13a91b2e1266641b862021ca5b7771a2d6
CSP <16367956+CSP197@users.noreply.github.com>
2023-11-10 12:32:50 -0800

    Cleaning up...

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ interface HeaderProps {
 
 function Header(props: HeaderProps) {
   let color = "indigo";
-  // let slidingClassName =
+  // let slidingClassName = // Alternative incase the className kept breaking tailwind for some reason...
   //   "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
   if (props.isSliding) {
     color = "violet";
